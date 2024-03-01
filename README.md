de8688b58bcd302b7d427fc47041d100c01081e9
Goapiai <operation@gocyber.world>
2024-03-01 13:03:19 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,24 @@
+# midjourney-state-machine
+This repo represents the generation task state machine of Midjourney, as late as November, 2023.
+
+## This repo contains three components:
+- A state machine graph build on [Plantuml](http://www.plantuml.com), stored in state_graph.iuml
+- A data model written in Golang, as the data structure of the states of the state machine, see /model/state.go
+- A set of checker functions written in Golang, it's used in [GoAPI's Midjourney API](https://www.goapi.ai/midjourney-api) for task state checking.
+
+## How to use
+Run the test program by executing `go run main.go`.
+
+The program will need you to enter an input. You can input one of the following:
+- `action`: This will execute the action on the last task.
+- `taskId::action`: This will execute the action on the specified task.
+- `quit`: This will exit the program.
+
+After entering an input, the program will process the action and display the results:
+- `parent task`: taskId, panState, action, actrionTree of parent task.
+- `new task`/`repeat task`: taskId and available actions of input action.
+
+You can input any action from the avilable actions list to continue.
+
+## Action state graph
+See `/graph` directory
---
+++ checker/action.go
@@ -0,0 +1,106 @@
+package checker
+
+import (
+	"fmt"
+	"sort"
+
+	"github.com/goapi-ai/midjourney-state-machine/model"
+	"golang.org/x/exp/slices"
+)
+
+/*
+ * check whether the action is already triggered or not
+ * action: user input action
+ * actionToTaskIdMap: taskId map for actions, key is action, value is taskId
+ */
+func CheckActionTriggered(action string, parentAction string, actionToTaskIdMap map[string]string) (taskId string, err error) {
+	// reroll action can be triggered multiple times, so it is not recorded in the map, but reroll still needs to be checked
+	if action == model.ActionReroll && AllowToReroll(parentAction) {
+		return "", nil
+	}
+	if taskId, ok := actionToTaskIdMap[action]; ok {
+		return taskId, nil
+	}
+	return "", fmt.Errorf("invalid action: %s", action)
+}
+
+/*
+ * create an empty map for action to taskId, used to record triggered taskId
+ */
+func CreateActionToTaskIdMap(actions []string) map[string]string {
+	actionToTaskIdMap := make(map[string]string, len(actions))
+	for _, action := range actions {
+		if action == model.ActionReroll {
+			continue
+		}
+		actionToTaskIdMap[action] = ""
+	}
+	return actionToTaskIdMap
+}
+
+func GetAvailableActionsFromMap(parentAction string, actionToTaskIdMap map[string]string) (actions []string) {
+	for action, taskId := range actionToTaskIdMap {
+		if taskId == "" {
+			actions = append(actions, action)
+		}
+	}
+	if AllowToReroll(parentAction) {
+		actions = append(actions, model.ActionReroll)
+	}
+	sort.Strings(actions)
+	return
+}
+
+func AllowToReroll(parentAction string) bool {
+	// all action results can be rerolled, except upscale
+	return !slices.Contains(model.ActionsUpscaleSeparate, parentAction) && !slices.Contains(model.ActionsUpscaleIncrease, parentAction)
+}
+
+/*
+ * get available action list
+ * panState: verticle if pan up/down was executed in history, horizontal if pan left/right was executed, cleared at outpaint action
+ */
+func GetActions(parentAction, action, panState string) ([]string, string) {
+	switch action {
+	case model.ActionImagine, model.ActionBlend:
+		fallthrough
+	case model.ActionVariation1, model.ActionVariation2, model.ActionVariation3, model.ActionVariation4:
+		fallthrough
+	case model.ActionVaryStrong, model.ActionVarySubtle:
+		fallthrough
+	case model.ActionInpaint:
+		fallthrough
+	case model.ActionOutpaint50, model.ActionOutpaint75, model.ActionOutpaintCustom, model.ActionOutpaintMakeSquare:
+		fallthrough
+	case model.ActionDescribe1, model.ActionDescribe2, model.ActionDescribe3, model.ActionDescribe4:
+		return model.State1Actions, ""
+	case model.ActionDescribe:
+		return model.State2Actions, ""
+	case model.ActionReroll:
+		if parentAction == model.ActionDescribe {
+			return model.State2Actions, ""
+		} else if slices.Contains(model.ActionsPan, parentAction) {
+			return model.State4Actions, panState
+		} else {
+			return model.State1Actions, ""
+		}
+	case model.ActionUpscale1, model.ActionUpscale2, model.ActionUpscale3, model.ActionUpscale4:
+		if panState == model.PanStateVertical {
+			return model.State5Actions, panState
+		} else if panState == model.PanStateHorizontal {
+			return model.State6Actions, panState
+		} else {
+			return model.State3Actions, panState
+		}
+	case model.ActionUpscale2x, model.ActionUpscale4x:
+		return model.State7Actions, ""
+	case model.ActionPanUp, model.ActionPanDown, model.ActionPanLeft, model.ActionPanRight:
+		if action == model.ActionPanUp || action == model.ActionPanDown {
+			return model.State4Actions, model.PanStateVertical
+		} else {
+			return model.State4Actions, model.PanStateHorizontal
+		}
+	default:
+		return []string{}, ""
+	}
+}
